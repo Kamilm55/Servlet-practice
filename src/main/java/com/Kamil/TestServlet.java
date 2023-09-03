f49f54d4274fd0df1089b405d906061e8f477463
Kamilm55 <kamilmmmdov2905@gmail.com>
2023-09-03 19:51:08 +0400

    UPLOAD NOT WORKING

--- src/main/java/com/Kamil/TestServlet.java
+++ src/main/java/com/Kamil/TestServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.servlet.ServletRequestContext;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +14,9 @@ import java.io.PrintWriter;
 public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+//            ServletRequestContext requestContext = new ServletRequestContext(req); Removed in jakarta servlet
+
         System.out.println("It works!!!");
         PrintWriter out = resp.getWriter();
         out.println("It works!!!");
--- src/main/java/com/Kamil/UploadFile.java
+++ src/main/java/com/Kamil/UploadFile.java
@@ -1,39 +1,36 @@
 package com.Kamil;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload2.*;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.RequestContext;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.commons.fileupload.servlet.ServletRequestContext;
+import jakarta.servlet.http.Part;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Collection;
 import java.util.List;
 
+@WebServlet("/UploadFile")
+@MultipartConfig
 public class UploadFile extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
-        try {
-            ServletRequestContext requestContext = new ServletRequestContext(req);
-            List<FileItem> fileItems = sf.parseRequest(requestContext);
-            for(FileItem file : fileItems){
-                file.write(new File("C:\\Users\\User\\IdeaProjects\\MavenProjects\\ServletApp\\uploads\\"+file.getName()));
-            }
-            PrintWriter out = resp.getWriter();
-            out.println("Uploaded succesfully");
-        } catch (FileUploadException e) {
-            throw new RuntimeException(e);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        String uploadPath = getServletContext().getRealPath("/uploads") ;
+        File uploadDir = new File(uploadPath);
 
+        if(!uploadDir.exists())uploadDir.mkdir();
+
+        Collection<Part> parts = req.getParts();
+        System.out.println(parts);
+        for(Part part : parts){
+            System.out.println("works");
+            String fileName = part.getSubmittedFileName();
+            part.write(uploadPath + File.separator + fileName);
+            System.out.println("downloaded");
+        }
 
     }
 }
